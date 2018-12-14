a4b72f155438456b45a8eb9aa16fca5c5f56a978
Uriel <urielbraga@hotmail.com>
2018-12-14 12:02:53 -0200

    Novas práticas ANGULAR 2

---
+++ Type Script/main.js
@@ -0,0 +1,9 @@
+var palavra = 'minha variavel';
+function soma(x, y) {
+    return x + y;
+}
+var array = [1, 3, 5, 7, 9];
+array.map(function (valor) {
+    return valor * 2;
+});
+array.map(function (valor) { return valor * 2; }); // Programacao Funcional em ES2015
---
+++ Type Script/main.ts
@@ -0,0 +1,15 @@
+var palavra = 'minha variavel';
+
+function soma(x,y){
+  return x + y ;
+}
+
+let array = [1,3,5,7,9]; 
+array.map(function (valor){ // Programacao Funcional em JS
+
+  return valor * 2;
+
+});
+
+array.map(valor  => valor * 2); // Programacao Funcional em ES2015
+                 
